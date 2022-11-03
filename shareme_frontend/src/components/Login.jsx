d3c66ae4052524fb755d403e9cee12a98c56a6ef
Sasha <s.marchuk95@gmail.com>
2022-11-03 16:58:36 +0100

    small fix

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -4,42 +4,46 @@ import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
-import { render } from "react-dom";
+
+import { client } from "../client";
 
 const Login = () => {
+  const navigate = useNavigate();
   const responseGoogle = (response) => {
-    localStorage.setItem("user", JSON.stringify(response.profilePbj));
-
-    const { name, googleId, imageUrl } = response.profilePbj;
-
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
     const doc = {
       _id: googleId,
       _type: "user",
-      userName: name,
+      userName: profile.name,
       image: imageUrl,
     };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
   };
 
   return (
     <div className="flex justify-start items-center flex-col h-screen">
-      <div className="relative w-full h-full">
+      <div className=" relative w-full h-full">
         <video
           src={shareVideo}
-          type="videp/mp4"
+          type="video/mp4"
           loop
           controls={false}
-          autoPlay
           muted
+          autoPlay
           className="w-full h-full object-cover"
         />
-        <div className="absolute flex flex-col justufy-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
           <div className="p-5">
-            <img src={logo} alt="logo" width="130px" />
+            <img src={logo} width="130px" />
           </div>
 
-          <div className="shadow-2x1">
+          <div className="shadow-2xl">
             <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_TOKEN}
+              clientId={`${process.env.REACT_APP_GOOGLE_TOKEN}`}
               render={(renderProps) => (
                 <button
                   type="button"
@@ -47,13 +51,12 @@ const Login = () => {
                   onClick={renderProps.onClick}
                   disabled={renderProps.disabled}
                 >
-                  <FcGoogle className="mr-4" />
-                  SignIn With Google
+                  <FcGoogle className="mr-4" /> Sign in with google
                 </button>
               )}
               onSuccess={responseGoogle}
               onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
+              cookiePolicy={"single_host_origin"}
             />
           </div>
         </div>
