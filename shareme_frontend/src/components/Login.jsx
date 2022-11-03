95f013e4ad24c404d9f1cb83395b7b0aa5f2d041
Sasha <s.marchuk95@gmail.com>
2022-11-03 17:43:53 +0100

    fixed login

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
 import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
+
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
@@ -10,8 +11,8 @@ import { client } from "../client";
 const Login = () => {
   const navigate = useNavigate();
   const responseGoogle = (response) => {
+    console.log(response);
     localStorage.setItem("user", JSON.stringify(response.profileObj));
-    console.log(response.profileObj);
     const { name, googleId, imageUrl } = response.profileObj;
     const doc = {
       _id: googleId,
@@ -43,22 +44,17 @@ const Login = () => {
           </div>
 
           <div className="shadow-2xl">
-            <GoogleLogin
-              clientId={`${process.env.REACT_APP_GOOGLE_TOKEN}`}
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="mr-4" /> Sign in with google
-                </button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy={"single_host_origin"}
-            />
+            {
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  console.log(credentialResponse);
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+                useOneTap
+              />
+            }
           </div>
         </div>
       </div>
